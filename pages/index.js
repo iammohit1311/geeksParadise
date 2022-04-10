0c7387c0ef072b77f6cdcaffd48649b0b6755980
Mohit Srivastava <iammohitpro@gmail.com>
2022-04-11 00:24:45 +0530

    safecommit

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+const Index = () => {
+    return (
+        <>
+        <h1 className="jumbotron text-center bg-primary square">Geeks Paradise</h1>
+        <p>from Next.js</p>
+        </>
+    )
+}
+
+export default Index
